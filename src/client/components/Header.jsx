886aebd68aa9c21ee1261348ec13918381b4a958
Anna Brooksby <johnhbrooksby@yahoo.com>
2023-05-16 16:10:30 -0600

    Header in place and top component taking shape

---
+++ src/client/components/Header.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <div className="header">
+      <div className="sub-header">
+        <div className="header-text">Let's Chat!</div>
+        <button className="header-btn">
+            <h6>Schedule a Call to Learn More</h6>
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
---
+++ src/client/components/Welcome.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Welcome = () => {
+  return (
+    <div className='welcome'>
+        <div>
+            
+        </div>
+        <div>
+
+        </div>
+            welcome
+    </div>
+  )
+}
+
+export default Welcome;
