29bea5d0e98ecf61b70bcb215da7620fbae3aa55
Anna Brooksby <johnhbrooksby@yahoo.com>
2023-06-01 17:09:57 -0600

    Deployment

--- src/server/main.js
+++ src/server/main.js
@@ -14,7 +14,7 @@ const app = express();
 app.use(express.json())
 app.use(cors())
 
-app.use(express.static(path.join(__dirname, "../../dist")))
+app.use(express.static(path.join(__dirname, "../../build")))
 
 
 User.hasMany(billingInfo)
