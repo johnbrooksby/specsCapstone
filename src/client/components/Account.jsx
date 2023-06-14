298060b71fbb723eacc96223ec01e429e566a6bb
Anna Brooksby <johnhbrooksby@yahoo.com>
2023-06-14 14:34:29 -0600

    temporarily removes stripe references

--- src/client/components/Account.jsx
+++ src/client/components/Account.jsx
@@ -1,12 +1,12 @@
 import React, {useContext} from 'react'
-import StripePayment from './Stripe'
+// import StripePayment from './Stripe'
 
 const Account = () => {
   return (
     <div className='testimonial accountPage'>
-      <StripePayment />
-        {/* <img src='https://i.gifer.com/7i16.gif' /> */}
-        {/* <h1 className='accountPageText'>- - - - Under Construction - - - -</h1> */}
+      {/* <StripePayment /> */}
+        <img src='https://i.gifer.com/7i16.gif' />
+        <h1 className='accountPageText'>- - - - Under Construction - - - -</h1>
     </div>
   )
 }
