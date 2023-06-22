41dd8fccfb730a336d3f418b596836268d4ccfa7
Anna Brooksby <johnhbrooksby@yahoo.com>
2023-06-22 17:05:46 -0600

    All client's bills appear on screen when client billing info is clicked

--- src/client/components/Billing.jsx
+++ src/client/components/Billing.jsx
@@ -5,20 +5,20 @@ const Billing = (props) => {
   console.log('bills in billing page', props.bills)
   console.log('client in billing page', props.client)
 
-  // const billList = props.bills.map((charge) => {
-  //   return (
-  //     <div>
-  //       {charge.charge_explanation}
-  //       {charge.amount_due}
-  //     </div>
-  //   );
-  // });
+  const billList = props.bills.map((charge) => {
+    return (
+      <div>
+        <h4>{charge.charge_explanation}</h4>
+        <h4>{charge.amount_due}</h4>
+      </div>
+    );
+  });
 
   return (
     <div>
       Update billing info for {props.client}
       <br></br>
-      {/* {billList} */}
+      {billList}
       {/* {props.bills.charge_explanation} */}
       {/* {props.bills.amount_due} */}
       <a onClick={() => props.setBack(!props.back)}>Back</a>
