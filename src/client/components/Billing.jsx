31bb776eaf230bd93d2e1296a092f4362bdb4fa2
Anna Brooksby <johnhbrooksby@yahoo.com>
2023-06-27 23:14:26 -0600

    Converts client billing info to a table

--- src/client/components/Billing.jsx
+++ src/client/components/Billing.jsx
@@ -4,13 +4,19 @@ const Billing = (props) => {
   // const { bills, client, back, setBack } = props;
   // console.log('bills in billing page', props.bills)
   // console.log('client in billing page', props.client)
+  let totalDue = 0;
 
   const billList = props.bills.map((charge) => {
+    totalDue += +charge.amount_due;
     return (
-        <div key={charge.id} className="billdiv">
-          <li>{charge.charge_explanation}</li>
-          <li>{charge.amount_due}</li>
-        </div>
+      // <div key={charge.id} className="billdiv">
+      //   <li>{charge.charge_explanation}</li>
+      //   <li>{charge.amount_due}</li>
+      // </div>
+      <tr>
+        <td className="bills_detail">{charge.charge_explanation}</td>
+        <td className="bills_detail">${charge.amount_due}</td>
+      </tr>
     );
   });
 
@@ -18,8 +24,25 @@ const Billing = (props) => {
     <div className="billdetail">
       <h3>Update billing info for {props.client}</h3>
       <br></br>
-      <div className="billing">
+      {/* <div className="billing">
         <ul className="billscontainer">{billList}</ul>
+      </div> */}
+      <div className="table">
+        <table className="bills_detail_table">
+          <thead>
+            <tr>
+              <td className="bills_detail_head">Explanation:</td>
+              <td className="bills_detail_head">Amount Due:</td>
+            </tr>
+          </thead>
+          <tbody>{billList}</tbody>
+          <tfoot>
+            <tr>
+              <td className="bills_detail_foot">Total Due:</td>
+              <td className="bills_detail_foot">${totalDue}</td>
+            </tr>
+          </tfoot>
+        </table>
       </div>
       {/* {props.bills.charge_explanation} */}
       {/* {props.bills.amount_due} */}
