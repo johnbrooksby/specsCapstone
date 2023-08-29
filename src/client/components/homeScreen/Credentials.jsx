d460b034145f2774cd093d8a8bfa812a2b88fcde
Anna Brooksby <johnhbrooksby@yahoo.com>
2023-08-29 12:15:31 -0600

    fixed spacing on credential logos

--- src/client/components/homeScreen/Credentials.jsx
+++ src/client/components/homeScreen/Credentials.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Credentials = () => {
   return (
     <div className='credential'>
-        <img src="https://drive.google.com/uc?export=view&id=1z79LANN3i4dkmNY3ZYSLn9c9qTwd3RiP" alt='cred' />
+        <img src="https://drive.google.com/uc?export=view&id=1matIyytarpRr6zHdUHONaxJLxkyZj6TC" alt='cred' />
     </div>
   )
 }
