67860cc32c08c5fd7449c27cf20a3e1d179a263b
Anna Brooksby <johnhbrooksby@yahoo.com>
2023-09-01 18:29:19 -0600

    Localized credential img

--- src/client/components/homeScreen/Credentials.jsx
+++ src/client/components/homeScreen/Credentials.jsx
@@ -1,11 +1,14 @@
-import React from 'react'
+import React from "react";
 
 const Credentials = () => {
   return (
-    <div className='credential'>
-        <img src="https://drive.google.com/uc?export=view&id=1matIyytarpRr6zHdUHONaxJLxkyZj6TC" alt='cred' />
+    <div className="credential">
+      <div className="credImg">
+        <img src="/creds.png" alt="credentials" />
+      </div>
+      {/* <img src="https://drive.google.com/uc?export=view&id=1matIyytarpRr6zHdUHONaxJLxkyZj6TC" alt='cred' /> */}
     </div>
-  )
-}
+  );
+};
 
-export default Credentials
+export default Credentials;
--- src/client/components/homeScreen/Welcome.jsx
+++ src/client/components/homeScreen/Welcome.jsx
@@ -1,19 +1,26 @@
-import React from 'react'
+import React from "react";
 
 const Welcome = () => {
   return (
-    <div className='welcome'>
-        <div>
-            <img className='welcome_img' src='/AI-ME-mottled-grey-background.png' alt='Sure Spark Coaching'/>
-        </div>
-        <div>
-
-        </div>
-        <h2>Let’s work together to bring back that spark into your life!</h2>
-        <a className='orange-btn' href="https://calendly.com/suresparkcoaching/30min?month=2023-05"
-            target="_blank">Schedule A Call To Learn More</a>
+    <div className="welcome">
+      <div>
+        <img
+          className="welcome_img"
+          src="/AI-ME-mottled-grey-background.png"
+          alt="Sure Spark Coaching"
+        />
+      </div>
+      <div></div>
+      <h2>Let’s work together to bring back that spark into your life!</h2>
+      <a
+        className="orange-btn"
+        href="https://calendly.com/suresparkcoaching/30min?month=2023-05"
+        target="_blank"
+      >
+        Schedule A Call To Learn More
+      </a>
     </div>
-  )
-}
+  );
+};
 
 export default Welcome;
