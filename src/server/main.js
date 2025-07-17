636881e87b443b3cf0990ea5d2e46a275a0cfbca
Anna Brooksby <johnhbrooksby@yahoo.com>
2025-07-16 21:46:57 -0600

    Removed non-working Stripe API commented out sections

--- src/server/main.js
+++ src/server/main.js
@@ -10,22 +10,13 @@ const {UserBackup} = require('./models/userBackup')
 const {BillingInfoBackup} = require('./models/billingBackup')
 const {PORT} = process.env
 const {register, login, usersAdmin, logout, billing, addbill, account, markaspaid, deleteuser, editUser} = require("./controllers/Auth")
-// const stripe = require('stripe')('sk_test_4eC39HqLyjWDarjtT1zdp7dc');
 
 const app = express();
 
 app.use(express.json())
 app.use(cors())
 
-// This is a public sample test API key.
-// Don’t submit any personally identifiable information in requests made with this key.
-// Sign in to see your own test API key embedded in code samples.
-// app.use(express.static('dist'));
-
 const YOUR_DOMAIN = 'http://localhost:5556';
-
-
-// app.use(express.static(path.resolve(__dirname, "../dist")))
 
 User.hasMany(BillingInfo)
 BillingInfo.belongsTo(User)
@@ -37,33 +28,11 @@ app.post('/api/login', login)
 app.get('/api/admin', usersAdmin)
 app.put('/api/logout', logout)
 app.post('/api/billing', billing)
-app.post("/api/addbill", addbill)
+app.post('/api/addbill', addbill)
 app.post('/api/account', account)
 app.put('/api/markaspaid', markaspaid)
 app.delete('/api/deleteuser/:client', deleteuser)
 app.put('/api/editUser', editUser)
-// app.post('/v1/checkout/sessions') 
-// app.post('/v1/checkout/sessions/:id/expire')
-// app.get('/v1/checkout/sessions/:id')
-// app.get('/v1/checkout/sessions')
-// app.get('/v1/checkout/sessions/:id/line_items')
-// app.post('/create-checkout-session', async (req, res) => {
-//   let price1 = 50
-//   const session = await stripe.checkout.sessions.create({
-//     line_items: [
-//       {
-//         // Provide the exact Price ID (for example, pr_1234) of the product you want to sell
-//         price: price1,
-//         quantity: 1,
-//       },
-//     ],
-//     mode: 'subscription',
-//     success_url: `${YOUR_DOMAIN}/success.html`,
-//     cancel_url: `${YOUR_DOMAIN}/cancel.html`,
-//   });
-
-//   res.redirect(303, session.url);
-// });
 
 // app.get('/*', function (req, res) {
 //   res.sendFile(path.join(__dirname, '../../dist', 'index.html'))
